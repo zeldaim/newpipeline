039ae0381467bdb4790f0d501fca11172cde576a
YHC <siuj@naver.com>
2025-11-27 09:00:12 +0000

    dd

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/home/ubuntu/cicd/newpipe/src/wr-home-top.jpg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="hello hihihi" />
 </template>
 
 <style scoped>
